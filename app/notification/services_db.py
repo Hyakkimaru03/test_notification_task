ca404b33b04ee112e517d82792eb2bf589136bc2
Hyakkimaru03 <muryg2003@gmail.com>
2026-01-16 16:35:34 +0700

    structure update: from functions-based to class-based, swagger Bearer Auth

--- app/notification/services_db.py
+++ app/notification/services_db.py
@@ -4,24 +4,6 @@ from notification.models import Notification
 from user.models import User
 
 
-async def user_exists(username: str) -> bool:
-    return await User.exists(username=username)
-
-
-async def create_user(username: str, password: str, avatar_url: Optional[str]) -> User:
-    return await User.create(
-        username=username, password=password, avatar_url=avatar_url
-    )
-
-
-async def get_user_by_username(username: str) -> Optional[User]:
-    return await User.get_or_none(username=username)
-
-
-async def get_user_by_id(uid: int) -> Optional[User]:
-    return await User.get_or_none(id=uid)
-
-
 async def create_notification(user: User, type_, text: Optional[str]) -> Notification:
     return await Notification.create(type=type_, text=text, user=user)
 
